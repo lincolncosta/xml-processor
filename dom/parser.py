bf1a3ebad58b114aa2f71fb63adec5e930e1e36a
tekpixo <lincoln.mcosta@gmail.com>
2020-07-08 18:41:44 -0300

    feat: parser funcionando com dom e etree.

--- dom/parser.py
+++ dom/parser.py
@@ -2,6 +2,7 @@ from xml.dom import minidom
 
 document = minidom.Document()
 authorsElement = document.createElement('authors')
+document.appendChild(authorsElement)
 
 datasource = open('../files/cf79.xml')
 parsed = minidom.parse(datasource)
@@ -11,5 +12,11 @@ authors = parsed.getElementsByTagName('AUTHOR')
 for author in authors:
     authorName = author.firstChild.nodeValue
 
-    authorElement = document.createElement('author').nodeValue = authorName
+    authorElement = document.createElement('author')
+    authorNameElement = document.createTextNode(authorName)
+    authorElement.appendChild(authorNameElement)
+
     authorsElement.appendChild(authorElement)
+
+with open('../files/authors.xml', 'w') as out:
+    document.writexml(out, encoding='utf-8')
